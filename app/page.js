5068ee4e4bca3290b6edb2734f19a6d3f78947a0
kachouenn <dahui9620@gmail.com>
2025-09-13 15:36:38 +0900

    2test comit

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <main style={{ textAlign: "center", padding: "2rem" }}>
       <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>凱旋門</h1>
       <p style={{ fontSize: "1.2rem", marginBottom: "2rem" }}>
-        本格フレンチレストランへようこそ
+        本格フレンチレストランへようこそoo
       </p>
       <nav style={{ fontSize: "1.1rem" }}>
         <a href="/menu" style={{ margin: "0 1rem" }}>
